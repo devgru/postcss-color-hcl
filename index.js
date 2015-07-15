016ab1e17dc850be50057443be02a0863c3beb2f
Dima Devgru <git@devg.ru>
2015-07-15 13:11:09 +0300

    refactor: minor prettification

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var postcss = require('postcss');
 var reduceFunctionCall = require('reduce-function-call');
-
 var d3 = require('d3-color');
 
 function justFloat(n) {
@@ -23,17 +22,19 @@ function transformDecl(decl) {
     function reduceHcl(body) {
         var hclValues = body.split(',').map(justFloat);
         var hclColor = d3.hcl.apply(null, hclValues);
-        if (!colorValuesDefined(hclColor))
+        if (!colorValuesDefined(hclColor)) {
             throw decl.error('Unable to parse color: "' + value + '"');
-        if (!hclColor.displayable())
+        }
+        if (!hclColor.displayable()) {
             throw decl.error('HCL color out of range: "' + value + '"');
+        }
+
         var hex = hclColor.toString();
         if (hclValues.length == 4) {
             var α = hclValues[3];
             var rgb = d3.rgb(hex);
             return ['rgba(', rgb.r, ', ', rgb.g, ', ', rgb.b, ', ', α, ')'].join('');
         }
-
         return hex;
     }
 
